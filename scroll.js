badf7bff1f560508f48bd946597b044dd9d235a9
Nikola <tomaha15@gmail.com>
2022-11-21 17:43:30 +0700

    Optimize IntersectionObserver code

--- scroll.js
+++ scroll.js
@@ -1,23 +1,11 @@
-window.addEventListener('DOMContentLoaded', setup);
+const content = document.querySelectorAll('.content');
 
-function setup() {
-    const options = {
-        rootMargin: '0px 0px -400px 0px'
-    }
+const observer = new IntersectionObserver (entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle('active', entry.isIntersecting)
+    } )
+},{threshold:1,})
 
-
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if(entry.isIntersecting){
-                entry.target.classList.add('active');
-                observer.unobserve(entry.target);
-            } else {
-                return;
-            }
-        })
-    }, options)
-
-    const content = document.querySelectorAll('.content');
-
-    content.forEach(content => observer.observe(content));
-}
+content.forEach(card => {
+    observer.observe(card)
+})
